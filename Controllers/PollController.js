1746f1de6fcf7f1b3e5f9d3cd47711b2c6efc50f
gustavomeloo <gugamelo2@gmail.com>
2022-11-28 22:31:49 -0300

    done

--- Controllers/PollController.js
+++ Controllers/PollController.js
@@ -59,7 +59,7 @@ class PollController {
           return
         }
 
-        const poll = {user, restaurant, data : new Date().toLocaleTimeString("pt-BR", {timeZone: 'America/Sao_Paulo'})}
+        const poll = {user, restaurant, time : new Date().toLocaleTimeString("pt-BR", {timeZone: 'America/Sao_Paulo'})}
 
 
         try {
